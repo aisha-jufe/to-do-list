06788dd1789122dc7f75909cb97f2ca5501786dd
aisha-jufe <aishajucfe287>
2023-01-11 10:28:47 +0300

    updated the components

---
+++ src/component/Todolist.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Todoitem from "./Todoitem";
+
+function Todolist(){
+    return(
+        <div>
+            <h1>welcome</h1>
+            <Todoitem />
+            <Todoitem />
+        </div>
+    )
+}
+export default Todolist;
